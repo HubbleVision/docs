6d166f48ab92230ea15ee0c2be6e752cb40dd4c5
Alex <alex@hubble.xyz>
2025-08-04 18:44:52 +0800

    feat: update base url

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,10 +15,12 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://docs.hubble.xyz',
+  url: 'https://hubblevision.github.io',
+  // url: 'https://docs.hubble-rpc.xyz',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/docs',
+  baseUrl: '/docs/',
+  // baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
